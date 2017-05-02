5612d842421783c2864e7c7e558ec9d13dee9abf
8281269367 <ajithkumar.ak95@gmail.com>
2017-05-02 16:43:08 +0530

    hi added person class

--- app/src/main/java/com/anjalil/admin/family/MainActivity.java
+++ app/src/main/java/com/anjalil/admin/family/MainActivity.java
@@ -3,6 +3,11 @@ package com.anjalil.admin.family;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.anjalil.admin.family.person.Marriage;
@@ -12,14 +17,62 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected EditText getName;
+    protected EditText getDate;
+    protected RadioGroup radioGroup;
+    protected RadioButton radioFemale;
+    protected RadioButton radioMale;
+    protected String gender;
+    protected Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Person a = new Person("AjithKumar A K",1995,"MALE");
-        Person b = new Person("emma watson",1992,"FEMALE");
+
+        Person a = new Person("AjithKumar A K",new Date(4),"MALE");
+        Person b = new Person("emma watson",new Date((4)),"FEMALE");
+
         Marriage marriage = new Marriage(a,b,new Date(2017,04,21),"Coavai");
         Log.i("HI",a.getName());
         Toast.makeText(MainActivity.this,marriage.getSpouse(a).getName()+" "+marriage.getSpouse(a).getGender(),Toast.LENGTH_LONG).show();
+
+        getName = (EditText)findViewById(R.id.nameEditText);
+        getDate = (EditText)findViewById(R.id.dateEditText);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGrp);
+        radioFemale = (RadioButton)findViewById(R.id.radioF);
+        radioMale = (RadioButton) findViewById(R.id.radioM);
+        button  = (Button) findViewById(R.id.submit_btn);
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = getName.getText().toString();
+                String date = getDate.getText().toString();
+
+                Toast.makeText(getApplicationContext(),name+date+gender,Toast.LENGTH_LONG).show();
+
+            }
+        });
+        
     }
-}
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioF:
+                if (checked)
+                        gender="FEMALE";
+                    break;
+            case R.id.radioM:
+                if (checked)
+                    gender="MALE";
+                    break;
+        }
+    }
+    }
